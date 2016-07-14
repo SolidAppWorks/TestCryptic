21c46a8889eb979d636acb757902a4a944d2fd89
ProjectMaster <saskuses@vivaldi.net>
2016-07-14 15:46:32 +0200

    Backup sync

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,42 @@
-#include<iostream>
+#include <stdio.h>
+#include <string>
 
 int main() {
+	
+	//Now, what do I do...
+	
+	/*
+	 * Pomysly na program
+	 * - szyfr jakiegos rodzaju popatrz stare zadania
+	 * 
+	 * */
+	
+/*int i;
 
-int i= 0;
+scanf("%d", &i);
+printf("LOL: %d\n",i* 1000);
+printf("Wow\n");
+*/
 
-std::cin>> i;
-std::cout<< i*1000<<"\n";
-std::cout<<"Wow\n";
+//Szyfr Viegenera
+
+//Tablica z alfabetem
+char alftab[26][26];
+std::string alfabet= "abcdefghijklmnopqrstuvwxyz";
+
+for(int i= 0; i<26; i++){
+	for(int j= 0; j<26; j++){
+		alftab[i][j]=alfabet[j];
+	}
+}
+
+for(int i= 0; i<26; i++){
+	printf("Rzad %d: ", i);
+	for(int j= 0; j<26; j++){
+		printf("%s", alftab[i][j]);
+	}
+	printf("\n");
+}
 
 return 0;
 }
