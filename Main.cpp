439aa6416f27e35f41b863edd9e9bccae77027fa
ProjectMaster <saskuses@vivaldi.net>
2016-07-16 14:08:58 +0200

    Cleaned, multi word working, added gitignore

--- Main.cpp
+++ Main.cpp
@@ -18,24 +18,46 @@ int checkPos(char k) {
  *  if result from checkpos == -1(white spaces) leave it be and go to the next one
  * but then iterator will not be good for klucz...
  * separate iterator for klucz?
+ * Done
  * */
 std::string szyfrowanie(std::string tekst, std::string klucz){
 	std::string szyfr= "";
+	int kit, kpos, tpos;
+	kit= kpos= tpos= 0;
+	
 		for(int i= 0; i< tekst.length(); i++){
-				szyfr+= alftab[checkPos(tekst[i% tekst.length()])][checkPos(klucz[i% klucz.length()])];
+			kpos= checkPos(klucz[i% klucz.length()]);
+			tpos= checkPos(tekst[i% tekst.length()]);
+			
+			if(tpos!= -1){
+				szyfr+= alftab[tpos][kpos];
+				kit++;
+				//printf("Did this\n");
+			}
+			else{
+				szyfr+= ' ';
+				//printf("Done that\n");
+				}
 		}
+		//printf("szyfr z funkcji to: %s, jego dlugosc to: %d\n", szyfr.c_str(), szyfr.length());
 		return szyfr;
 }
 	
-std::string deszyfrowanie(std::string szyfr, std::string klucz){
+std::string deszyfrowanie(std::string encode, std::string klucz){
 	std::string deszyfr= "";
-	for(int i= 0; i< szyfr.length(); i++){
+	for(int i= 0; i< encode.length(); i++){
 		int kpos= checkPos(klucz[i% klucz.length()]);
-		for(int j= 0; j< 26; j++){
-			if(alftab[kpos][j]== szyfr[i])
-			deszyfr+= alfabet[j];
+		if(encode[i]!= ' '){
+			for(int j= 0; j< 26; j++){
+				if(alftab[kpos][j]== encode[i])
+				deszyfr+= alfabet[j];
+			}
+		}
+		else{
+			deszyfr+= ' ';
 		}
 	}
+	//printf("deszyfr z funkcji to: %s, jego dlugosc to: %d\n", deszyfr.c_str(), deszyfr.length());
 		return deszyfr;
 }
 
@@ -63,7 +85,7 @@ for(int i= 0; i<26; i++){
 
 
 printf("Prosze podac tekst do zaszyfrowania: ");
-std::cin>> slowojawne;
+std::getline(std::cin, slowojawne);
 
 printf("Prosze podac klucz: ");
 std::cin>> klucz;
